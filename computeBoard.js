b039975ffff381c92755f9347f39aecd99cd32ed
Tyler Campaigne <tyler@vidyard.com>
2019-11-28 17:52:43 -0500

    fixing isOpen reading for class matrix and renaming it to be clearer

--- computeBoard.js
+++ computeBoard.js
@@ -3,36 +3,36 @@ class Board {
     this.width = json.width
     this.height = json.height
 
-    this.board = new Array(json.width)
-    for (var i = this.board.length - 1; i >= 0; i--) {
-      this.board[i] = new Array(json.height)
+    this.matrix = new Array(json.width)
+    for (var i = this.matrix.length - 1; i >= 0; i--) {
+      this.matrix[i] = new Array(json.height)
     }
 
     // Food pieces
     for (let food of json.food) {
-      this.board[food.x][food.y] = 'f'
+      this.matrix[food.x][food.y] = 'f'
     }
 
-    // All snakes on this.board
+    // All snakes on this.matrix
     for (let snake of json.snakes) {
       for (let part of snake.body) {
-        this.board[part.x][part.y] = 's'
+        this.matrix[part.x][part.y] = 's'
       }
       
-      this.board[snake.body[0].x][snake.body[0].y] = 'S'
+      this.matrix[snake.body[0].x][snake.body[0].y] = 'S'
     }
 
     // Label where I am
     for (let part of me.body) {
-      this.board[part.x][part.y] = 'm'
+      this.matrix[part.x][part.y] = 'm'
     } 
-    this.board[me.body[0].x][me.body[0].y] = 'M'
+    this.matrix[me.body[0].x][me.body[0].y] = 'M'
   }
 
   log () {
-    for (var i = 0; i < this.board[0].length; i++) {
+    for (var i = 0; i < this.matrix[0].length; i++) {
       let rowOutput = ''
-      for (let column of this.board) {
+      for (let column of this.matrix) {
         const char = column[i] || '.'
         rowOutput += `${char} `
       }
@@ -40,12 +40,13 @@ class Board {
     }
   }
 
-  isOpen (proposed, board)  {
+  isOpen (proposed)  {
+    console.log(proposed)
     return proposed.x >= 0
       && proposed.y >= 0
       && proposed.x < this.width
       && proposed.y < this.height
-      && (board[proposed.x][proposed.y] == 'f' || typeof board[proposed.x][proposed.y] == 'undefined')
+      && (this.matrix[proposed.x][proposed.y] == 'f' || typeof this.matrix[proposed.x][proposed.y] == 'undefined')
   }
 }
 
