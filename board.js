99bcb35a3a7162e6a0df457a6ff252885e2223fe
Tyler Campaigne <tyler@vidyard.com>
2019-11-28 22:46:52 -0500

    I'm dumb, enemy head's aren't valid squares, so their score never worked

--- board.js
+++ board.js
@@ -90,12 +90,12 @@ class Board {
 
   spaceScore (proposed) {
     let score = 0
-    if (!this.isValid(proposed)) {
+    if (this.isEnemy(proposed)) {
+      score = -1000
+    } else if (!this.isValid(proposed)) {
       score = 0
     } else if (this.isFood(proposed)) {
       score = 100 + (200 * ((100 - this.health) / 100)^(100 - this.health))
-    } else if (this.isEnemy(proposed)) {
-      score = -10000
     } else if (this.isOpen(proposed)) {
       score = 100
     }
